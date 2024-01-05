00be998e965cebc919d8a1285d394f64cbb7a84d
Trevor Davis <trevor@santesuite.com>
2024-01-05 14:06:42 -0500

    Added license header to source files.

--- SanteDB.Client.Win/App.xaml.cs
+++ SanteDB.Client.Win/App.xaml.cs
@@ -1,4 +1,24 @@
-﻿using Microsoft.Data.Sqlite;
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: trevor
+ * Date: 2023-4-19
+ */
+using Microsoft.Data.Sqlite;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -117,7 +137,7 @@ namespace SanteDB.Client.WinUI
                         Name = "org.santedb.disconnected_client.win32"
                     };
 
-                    var directoryprovider = new Shared.LocalAppDirectoryProvider();
+                    var directoryprovider = new Shared.LocalAppDirectoryProvider("dc-win32");
 
                     SanteDB.Client.Batteries.ClientBatteries.Initialize(directoryprovider.GetDataDirectory(), directoryprovider.GetConfigDirectory(), new Configuration.Upstream.UpstreamCredentialConfiguration()
                     {
